8589ecd2720c238757a11f82bb8cdc7b09842106
choimory <choimory@naver.com>
2021-06-27 17:20:59 +0900

    Initial commit

---
+++ src/main/java/com/choimory/helloresttemplate/HelloRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.helloresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloRestTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/helloresttemplate/HelloRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.helloresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
